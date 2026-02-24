701253ba4fe7577179577e91986f6a69be938b1f
SirOogway <jesusandresrodriguezgarcia@outlook.com>
2026-02-24 16:07:40 -0500

    wip: dynamic padding logic for MotionToggleButton
    working on dynamic padding to keep second content outside button bounds
    implementation not finished yet

--- composemotionui/src/main/java/com/xnovacompany/composemotionui/motiontogglebutton/MotionToggleButton.kt
+++ composemotionui/src/main/java/com/xnovacompany/composemotionui/motiontogglebutton/MotionToggleButton.kt
@@ -1,27 +1,52 @@
 package com.xnovacompany.composemotionui.motiontogglebutton
 
+import android.content.res.Resources
+import android.util.Log
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 
 @Composable
 fun MotionToggleButton(
+//    onClick: ()-> Unit,
     contentOn: @Composable () -> Unit,
     contentOff: @Composable () -> Unit,
     modifier: Modifier = Modifier,
@@ -30,11 +55,48 @@ fun MotionToggleButton(
     val scrollState = rememberScrollState()
     val coroutinScope = rememberCoroutineScope()
 
+    var heightDp by remember { mutableStateOf(0.dp) }
+    var gapDp by remember { mutableStateOf(0.dp) }
+    val density = LocalDensity.current
+
+    /*    var isOn by remember { mutableStateOf(false) }
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Gray)
+                .height(40.dp)
+                .clickable { isOn = !isOn },
+            contentAlignment = Alignment.Center
+        ) {
+            AnimatedContent(
+                targetState = isOn,
+                transitionSpec = {
+                    slideInVertically { height -> height } togetherWith
+                            slideOutVertically { height -> -height }
+                },
+                label = ""
+            ) { state ->
+                if (state) {
+                    contentOn()
+                } else {
+                    contentOff()
+                }
+            }
+        }*/
+
+
     Box(
-        modifier = Modifier
-            .background(Color.Red)
-            .height(45.dp)
-            .width(170.dp)
+        modifier = modifier
+            .fillMaxWidth() // Ocupar la mitad de la altura por ser una tupla de contenido
+            .height(80.dp)
+            .onGloballyPositioned { coordinates ->
+                heightDp = with(density) {
+                    coordinates.size.height.toDp()
+                }
+                gapDp = heightDp/2
+                Log.d("HEIGHT", "${heightDp}")
+            }
             .clickable {
                 coroutinScope.launch() {
                     val target =
@@ -54,11 +116,10 @@ fun MotionToggleButton(
         if (scrollDirection == Orientation.Vertical)
             Column(
                 modifier = Modifier
-//                    .padding(0.dp)
                     .wrapContentSize()
                     .background(Color.Gray)
                     .then(scrollModifier),
-//                verticalArrangement = Arrangement.spacedBy(110.dp)
+                verticalArrangement = Arrangement.spacedBy(gapDp)
             ) { contentOn(); contentOff() }
         else Row(modifier = modifier.then(scrollModifier)) { contentOn(); contentOff() }
     }
--- sample/src/main/java/com/xnovacompany/composemotionui/sample/MainActivity.kt
+++ sample/src/main/java/com/xnovacompany/composemotionui/sample/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.xnovacompany.composemotionui.motiontogglebutton.MotionToggleButton
 import com.xnovacompany.composemotionui.sample.ui.theme.ComposeMotionUITheme
@@ -23,14 +29,28 @@ class MainActivity : ComponentActivity() {
             ComposeMotionUITheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Column(
-                        modifier = Modifier.fillMaxSize(),
+                        modifier = Modifier.fillMaxSize().background(Color.Gray),
                         verticalArrangement = Arrangement.Center,
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
                         MotionToggleButton(
-                            { Text("Dark", fontSize = 40.sp) },
-                            { Text("Dark", fontSize = 40.sp) },
-//                            { Text("Light", fontSize = 40.sp) },
+//                            { Text("Dark", fontSize = 40.sp) },
+//                            { Text("Dark", fontSize = 40.sp) },
+                            {
+                                Box(modifier = Modifier
+                                    .height(40.dp)
+                                    .width(40.dp)
+                                    .background(Color.Red)
+                                ){}
+                            },
+                            {
+                                Box(
+                                    modifier = Modifier
+                                        .height(40.dp)
+                                        .width(40.dp)
+                                        .background(Color.Blue)
+                                ) {}
+                            }
                         )
                     }
                 }
